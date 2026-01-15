cb298f043f9078c91998653c0476edb3a08d8039
thanhhq8194-spec <thanhhq8194@ut.edu.vn>
2026-01-15 12:22:56 +0700

    Update app.py

--- Flask-CleanArchitecture-main/Flask-CleanArchitecture-main/src/app.py
+++ Flask-CleanArchitecture-main/Flask-CleanArchitecture-main/src/app.py
@@ -11,7 +11,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 
 def create_app():
-    app = Flask(__name__) #alo
+    app = Flask(__name__) 
     Swagger(app)
     # Đăng ký blueprint trước
     app.register_blueprint(todo_bp)
